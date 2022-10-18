8b3a4a0acf23123c2d54daf14a512c1bef32057d
praxis <chesnokovprm@gmail.com>
2022-10-18 17:51:18 +0600

    outside_click_popup_close_done

--- src/pages/index.js
+++ src/pages/index.js
@@ -105,3 +105,42 @@ addEventListener("keydown", function eventHandler(evt) {
     closeModals();
   }
 });
+
+pictureViewerPopup.addEventListener("click", function handleClick(e) {
+  const pictureViewer = document.querySelector(".picture-viewer__pic");
+  if (e.target !== pictureViewer) closeModals();
+});
+
+profileUpdaterPopup.addEventListener("mouseup", function handleClick(e) {
+  const editWindow = document.querySelector(".edit-window");
+  const editWindowInputForm = document.querySelector(
+    ".edit-window__input-form"
+  );
+  const editWindowTitle = document.querySelector(".edit-window__title");
+  console.log(e.target);
+  if (
+    e.target !== editWindow &&
+    e.target !== editWindowInputForm &&
+    e.target !== nameInput &&
+    e.target !== jobInput &&
+    e.target !== editWindowTitle
+  )
+    closeModals();
+});
+
+placeAdderPopup.addEventListener("mouseup", function handleClick(e) {
+  const editWindows = document.querySelectorAll(".edit-window");
+  const editWindowInputForms = document.querySelectorAll(
+    ".edit-window__input-form"
+  );
+  const editWindowTitles = document.querySelectorAll(".edit-window__title");
+  console.log(e.target);
+  if (
+    e.target !== editWindows[1] &&
+    e.target !== editWindowInputForms[1] &&
+    e.target !== placeInput &&
+    e.target !== placeLinkInput &&
+    e.target !== editWindowTitles[1]
+  )
+    closeModals();
+});
