ae4d39d25d2f4675840433163c379fd868b25ba2
praxis <chesnokovprm@gmail.com>
2022-10-19 15:36:45 +0600

    1st_feedback_validation_modularized

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,10 +27,13 @@ import {
   pictureViewerPopup,
   renderCard,
 } from "../components/card";
+import { enableValidation } from "../components/validation";
 
 initialCards.reverse().forEach((cardinfo) => {
   renderCard(cardinfo.name, cardinfo.link);
 });
+
+enableValidation();
 
 // popup update details
 
@@ -41,69 +44,6 @@ function updateProfile(evt) {
   userJob.textContent = jobInput.value;
   closePopup(profileUpdaterPopup);
 }
-
-// validation
-
-const showInputError = (formElement, inputElement, errorMessage) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add("edit-window__input-string_mistake");
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add("edit-window__mistake-message_active");
-};
-
-const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove("edit-window__input-string_mistake");
-  errorElement.classList.remove("edit-window__mistake-message_active");
-};
-
-const isValid = (formElement, inputElement) => {
-  if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
-  } else {
-    hideInputError(formElement, inputElement);
-  }
-};
-
-const setEventListeners = (formElement) => {
-  const inputList = Array.from(
-    formElement.querySelectorAll(".edit-window__input-string")
-  );
-  const buttonElement = formElement.querySelector(".edit-window__submit");
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener("input", () => {
-      isValid(formElement, inputElement);
-      toggleButtonState(inputList, buttonElement);
-    });
-  });
-};
-
-const enableValidation = () => {
-  const formList = Array.from(
-    document.querySelectorAll(".edit-window__input-form")
-  );
-  formList.forEach((formElement) => {
-    setEventListeners(formElement);
-  });
-};
-
-enableValidation();
-
-const hasInvalidInput = (inputList) => {
-  return inputList.some((inputElement) => {
-    return !inputElement.validity.valid;
-  });
-};
-
-const toggleButtonState = (inputList, buttonElement) => {
-  if (hasInvalidInput(inputList)) {
-    buttonElement.disabled = true;
-    buttonElement.classList.add("edit-window__submit_inactive");
-  } else {
-    buttonElement.disabled = false;
-    buttonElement.classList.remove("edit-window__submit_inactive");
-  }
-};
 
 // viewing posts listeners
 
